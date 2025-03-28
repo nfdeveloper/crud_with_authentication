2fac4a347f1794c09e0c2f1c5893af4114db9618
nfdeveloper <nfdeveloper04@gmail.com>
2025-03-28 11:40:29 -0300

    feat: creating user objects.

--- src/configuration/rest_err/rest_err.go
+++ src/configuration/rest_err/rest_err.go
@@ -1,4 +1,4 @@
-package configuration
+package resterr
 
 import "net/http"
 
--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,5 +1,24 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
 
-func CreateUser(c *gin.Context) {}
+	"github.com/gin-gonic/gin"
+	resterr "github.com/nfdeveloper/crud_with_authentication/src/configuration/rest_err"
+	"github.com/nfdeveloper/crud_with_authentication/src/controller/model/request"
+)
+
+func CreateUser(c *gin.Context) {
+
+	var userRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		restErr := resterr.NewBadRequestError(
+			fmt.Sprintf("There are some incorret fields, erro=%s ", err.Error()))
+
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+
+	fmt.Println(userRequest)
+}
