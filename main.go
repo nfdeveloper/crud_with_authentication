0b205ea7efe9909a35c0d46979ab05135adc2dc9
nfdeveloper <nfdeveloper04@gmail.com>
2025-03-28 00:36:58 -0300

    feat: creating the application routes.

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"os"
 
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"github.com/nfdeveloper/crud_with_authentication/src/controller/routes"
 )
 
 func main() {
@@ -15,6 +15,12 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
-	fmt.Println(os.Getenv("TEST"))
+	router := gin.Default()
+
+	routes.InitRoutes(&router.RouterGroup)
+
+	if err := router.Run(":8080"); err != nil {
+		log.Fatal(err)
+	}
 
 }
